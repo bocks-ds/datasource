6c2e38a816eb25ca6b7294e5e6f27df0360ab9aa
Knight, Eddie <iv.eddieknight@gmail.com>
2020-03-24 10:09:40 -0400

    Refactored resolver functions into objects

--- code/index.js
+++ code/index.js
@@ -1,18 +1,23 @@
 const { ApolloServer, gql } = require('apollo-server');
 
 const databaseInstance = require('database/builder')
-const typeDefs = require('definitions/builder')
+const DefinitionsBuilder = require('definitions/builder')
 const ResolversBuilder = require('resolvers/builder')
 const env = require('env_vars')
 
-databaseInstance.initialize().then(response => {
+databaseInstance.initialize().then(_ => {
+    const typeDefs = new DefinitionsBuilder().get_type_defs() // must run prior to ResolversBuilder
     const resolvers = new ResolversBuilder()
 
     env.logger.info("Preparing Apollo Server...")
+
     const server = new ApolloServer({typeDefs, resolvers})
+
     env.logger.info("Apollo Server ready to launch.")
     env.logger.info("Launching GraphQL via Apollo Server...")
+
     server.listen({ port: env.port }).then(({ url }) => {
         env.logger.info(`🚀  Apollo Server ready at ${url}.`);
     });
+
 }).catch(error =>{env.logger.info(error)})
